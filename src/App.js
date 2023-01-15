f1bc658fb8a13f9c0f4778f2d79bda1f35558693
Neta Dror <neta.l.dror@gmail.com>
2023-01-15 15:05:17 +0200

    added basic components and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-code
 import './App.css';
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p> Hello
-        </p>
-      </header>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
